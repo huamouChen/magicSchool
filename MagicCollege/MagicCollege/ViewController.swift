3adcbccfd496eeaab851123d94a42a3059805b7f
陈华谋 <137983656@qq.com>
2017-11-15 16:53:36 +0800

    creat project

---
+++ MagicCollege/MagicCollege/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MagicCollege
+//
+//  Created by HuaMou.Chen on 2017/11/15.
+//  Copyright © 2017年 sito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
